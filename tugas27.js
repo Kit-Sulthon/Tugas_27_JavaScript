2087c0d271792aa21a23f2745ebe215f8755d374
T R A $ H <sulthons.a19@gmail.com>
2023-10-06 08:41:44 +0700

    Tugas 27 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,17 @@
+function tugas27(){
+    var Alt1 = "No";
+    var Alt2 = "rumah saya";
+    var Alt3 = 30;
+    var Alt4 = "A";
+
+    var Alt5 = "Jalan pangeran Tritayasa No";
+    var Alt6 = 70; 
+
+    hasil1 = String(Alt3);
+    hasil2 = String(Alt6);
+
+    console.log(Alt1.concat(', ', Alt2, ', ', hasil1, Alt4));
+    console.log(Alt5.concat(', ', hasil2));
+}
+
+tugas27()
